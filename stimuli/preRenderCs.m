ede12f43698d4bbf59e29d22558e189022f793e4
Alasdair Clarke <a.clarke@abdn.ac.uk>
2015-11-10 10:30:33 +0000

    made items larger

--- stimuli/preRenderCs.m
+++ stimuli/preRenderCs.m
@@ -8,7 +8,7 @@ params.targDisplayTime = 0.1;
 
 params.bkgrndColour = 125;
 params.delta = 280;
-params.N = 64; %determines spacing between adjacent boxes
+params.N = 128; %determines spacing between adjacent boxes
 params.w = 6;
 params.letterSpace = 15;
 
@@ -16,13 +16,13 @@ params.flankerIntensity = 225;
 params.targetIntensity  = 225;
 
 params.boxW = 2;
-params.boxN = 48;
+params.boxN = 72;
 params.boxColour = [50, 50, 50];
 params.tboxColourSacc = [175, 25, 50];
 params.tboxColourFix = [25, 50, 200];
 
-params.Cr1 = 12;
-params.Cr2 = 8;
+params.Cr1 = 28;
+params.Cr2 = 16;
 params.Cw = 8;
 params.Cphi = [0 90, 180, 270];
 params.CphiLabels = {'up', 'left','down', 'right'};
